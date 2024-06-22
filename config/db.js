666b7194ad5b253076aef1d99adeafc25c3f3aa3
brijesh <oddsworld0121@gmail.com>
2024-06-22 16:44:39 +0530

    update db.js

--- config/db.js
+++ config/db.js
@@ -5,12 +5,7 @@ mongoose.set("strictQuery", false);
 const connectDB = async () => {
     try {
 
-        let connection_url = process.env.MONGO_DB_CONNECTION_URL_STRING;
-        const mongo_password = process.env.MONGO_DB_PASSWORD;
-        const mongo_ursername = process.env.MONGO_DB_USERNAME;
-
-        connection_url = connection_url.replace("<username>", mongo_ursername);
-        connection_url = connection_url.replace("<password>", mongo_password);
+        const connection_url = process.env.MONGO_DB_CONNECTION_URL_STRING;
 
 
         const connection = await mongoose.connect(connection_url)
